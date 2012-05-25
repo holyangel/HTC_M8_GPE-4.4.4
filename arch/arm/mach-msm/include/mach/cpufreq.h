31ed907c00eb4a49d98349b6f26809eb84dc139c
Praveen Chidambaram <pchidamb@codeaurora.org>
2012-05-25 17:29:11 -0600

    msm: cpufreq: Add API to allow limiting of min and max cpu frequencies
    CPUFreq provides scaling sysfs nodes to set min and max frequencies, but
    there are times when the operating range has to be controlled
    irrespective of the scaling limits, to ensure the thermal stability of
    the device.
    
    The API allows kernel thermal safeguards to pitch in and limit the
    frequencies.
    
    Change-Id: I3dc7496747792e6223695131f1644dd87054b025
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c
    
    Conflicts:
    	arch/arm/mach-msm/include/mach/cpufreq.h

--- arch/arm/mach-msm/include/mach/cpufreq.h
+++ arch/arm/mach-msm/include/mach/cpufreq.h
@@ -10,6 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+
 #ifndef __MACH_CPUFREQ_H
 #define __MACH_CPUFREQ_H
 
@@ -37,3 +38,34 @@ extern unsigned long msm_cpufreq_get_bw(void)
 #endif
 
 #endif
+
+#ifndef __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H
+#define __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H
+
+#define MSM_CPUFREQ_NO_LIMIT 0xFFFFFFFF
+
+#ifdef CONFIG_CPU_FREQ_MSM
+
+/**
+ * msm_cpufreq_set_freq_limit() - Set max/min freq limits on cpu
+ *
+ * @cpu: The cpu core for which the limits apply
+ * @max: The max frequency allowed
+ * @min: The min frequency allowed
+ *
+ * If the @max or @min is set to MSM_CPUFREQ_NO_LIMIT, the limit
+ * will default to the CPUFreq limit.
+ *
+ * returns 0 on success, errno on failure
+ */
+extern int msm_cpufreq_set_freq_limits(
+		uint32_t cpu, uint32_t min, uint32_t max);
+#else
+static inline int msm_cpufreq_set_freq_limits(
+		uint32_t cpu, uint32_t min, uint32_t max)
+{
+	return -ENOSYS;
+}
+#endif
+
+#endif /* __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H */
